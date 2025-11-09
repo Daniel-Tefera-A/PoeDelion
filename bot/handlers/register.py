680089447d743672aa5150f5d4fc433f012fdac1
Daniel Tefera <dayuzuel@gmail.com>
2025-11-09 13:15:08 -0800

    upload

---
+++ bot/handlers/register.py
@@ -0,0 +1,78 @@
+from os import name
+import re
+from turtle import update #to validate regular expressions
+from telegram import Update, KeyboardButton, ReplyKeyboardMarkup #for the share contact button
+
+from telegram.ext import CommandHandler, MessageHandler, ConversationHandler, filters, ContextTypes
+from bot.utils.db import add_user
+
+
+ASK_NAME, ASK_PHONE = range(2)
+
+# Validate name — only letters and spaces allowed
+def is_valid_name(name: str) -> bool:
+    return bool(re.match(r"^[A-Za-z\s]{2,30}$", name))
+
+# Validate phone number — numbers with optional "+"
+def is_valid_phone(phone: str) -> bool:
+    return bool(re.match(r"^\+?\d{7,15}$", phone))
+
+async def start_registration(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text("📝 Please enter your full name:")
+    return ASK_NAME
+# a separate function for getting and validating name
+async def get_name(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    user_name = update.message.text.strip()
+
+    if not is_valid_name(user_name):
+        await update.message.reply_text("❌ Please enter a valid name (letters only, no numbers).")
+        return ASK_NAME
+
+    context.user_data["name"] = user_name
+
+    contact_button = KeyboardButton("Share phone number", request_contact=True)
+    reply_markup = ReplyKeyboardMarkup([[contact_button]], one_time_keyboard=True)
+    await update.message.reply_text("✅ Great! Now share or enter your phone number:", reply_markup=reply_markup)
+    return ASK_PHONE
+# a separate function for getting and validating phone number
+async def get_phone(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    # If shared via button
+    if update.message.contact:
+        phone = update.message.contact.phone_number
+    else:
+        phone = update.message.text.strip()
+
+    if not is_valid_phone(phone):
+        await update.message.reply_text("❌ Invalid phone number. Use format +123456789 or 0123456789.")
+        return ASK_PHONE
+
+    context.user_data["phone"] = phone
+    name = context.user_data["name"]
+
+    add_user(name, phone)
+# directly display that registration is complete no separate function needed
+
+    print(f"✅ Registration complete!\n\nName: {name}\nPhone: {phone}")
+
+    await update.message.reply_text(
+    f"✅ Registration complete!\n\nName: {name}\nPhone: {phone}"
+)
+
+    return ConversationHandler.END
+
+async def cancel(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text("❌ Registration cancelled.")
+    return ConversationHandler.END
+
+# Register handler for main.py
+register_handler = ConversationHandler(
+    entry_points=[CommandHandler("register", start_registration)],
+    states={
+        ASK_NAME: [MessageHandler(filters.TEXT & ~filters.COMMAND, get_name)],
+        ASK_PHONE: [
+            MessageHandler(filters.CONTACT, get_phone),
+            MessageHandler(filters.TEXT & ~filters.COMMAND, get_phone),
+        ],
+    },
+    fallbacks=[CommandHandler("cancel", cancel)],
+)
---
+++ bot/main.py
@@ -0,0 +1,65 @@
+import sys
+import os
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+from dotenv import load_dotenv
+from telegram import Update
+from telegram.ext import ApplicationBuilder, CommandHandler, MessageHandler, filters, ContextTypes
+from telegram.request import HTTPXRequest
+from bot.handlers.register import register_handler
+from telegram import ReplyKeyboardMarkup
+from  bot.utils.db import load_data
+
+load_dotenv()
+TOKEN = os.getenv("TELEGRAM_TOKEN")
+
+async def check(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    user = update.effective_user
+    data = load_data()
+    user_name = user.first_name
+    found_entries = [entry for entry in data if entry["name"] == user_name]
+    if found_entries: 
+        entries_text = "".join([f'Name: {e["name"]}, Phone: {e["phone"]}' for e in found_entries])
+        await update.message.reply_text(f"You are registered: {entries_text}")
+    else:
+        await update.message.reply_text("You are not registered yet. Use /regiser to register")
+
+    
+
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    keyboard = [["/register", "/check"]]
+    reply_markup = ReplyKeyboardMarkup(keyboard, one_time_keyboard=True, resize_keyboard=True)
+    await update.message.reply_text(
+        "👋 Welcome to the Registration Bot!\n\nUse the buttons below to register or check your info", reply_markup=reply_markup
+    )
+
+async def help_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    keyboard = [["/reister", "/check"]]
+    reply_markup = ReplyKeyboardMarkup(keyboard, one_time_keyboard=True, resize_keyboard=True)
+    await update.message.reply_text(
+        "Welcome! This is a simple registration bot!\n"
+        "Use the buttons or these commands:\n"
+        "/start - Restart the bot.\n"
+        "/register - Enter your information and join.\n"
+        "/check - Check your information if you have registred.\n "
+        "Thank you for using our bot!😊"
+    )
+
+def main():
+    request = HTTPXRequest(connect_timeout=20.0, read_timeout=20.0)
+    app = ApplicationBuilder().token(TOKEN).request(request).build()
+
+    app.add_handler(CommandHandler("start", start))
+    app.add_handler(CommandHandler("help", help_command))
+    app.add_handler(CommandHandler("check", check))
+    app.add_handler(register_handler)
+
+
+
+    print("✅ Bot is running...")
+    app.run_polling()
+
+if __name__ == "__main__":
+    main()
+
+
+
